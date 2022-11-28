875188176a01d5df7d132a591903943f2f7540bd
CLOZR <62227342+modilzo@users.noreply.github.com>
2022-11-28 20:55:06 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var table = document.querySelector("#table");
+var b1 = document.querySelector(".section-1");
+var b2 = document.querySelector(".section-2");
+var b3 = document.querySelector(".section-3");
+var b4 = document.querySelector(".section-4");
+var b5 = document.querySelector(".section-5");
+var b6 = document.querySelector(".section-6");
+var b7 = document.querySelector(".section-7");
+var b8 = document.querySelector(".section-8");
+var b9 = document.querySelector(".section-9");
+
+b1.addEventListener("click" , function(){
+    
+});
+
+b2.addEventListener("click" , function(){
+
+});
